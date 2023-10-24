175a2924bc311f044cf8fdee6c8cba59ce78ad27
Lab 308 <Lab 308@DESKTOP-448EC3Q>
2023-10-24 16:22:02 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectRed
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void button1_MouseMove(object sender, MouseEventArgs e)
+		{
+			Random rnd =  new Random();
+			button1.BackColor = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+			button1.Location = new Point(rnd.Next(816), rnd.Next(489));
+		}
+	}
+}
